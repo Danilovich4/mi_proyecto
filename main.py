1c26dbcf869b7d7db58a719118272da4a9933fda
Danilovich4 <121514623+Danilovich4@users.noreply.github.com>
2024-12-15 12:53:36 +0100

    Retorrnar version 10

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 """
-Crea una función llamada mensaje() que imprima "Hola, mundo".
-Crea un decorador que modifique la función para que:
-Imprima "Inicio de la función" antes de ejecutarse.
-Imprima "Fin de la función" después de ejecutarse.
-Aplica el decorador usando @decorar_funcion.
-Ejecuta la función decorada y verifica el comportamiento.
+Crea un módulo llamado operaciones.py con las siguientes funciones:
+multiplicar(a, b)
+dividir(a, b) (maneja la división por cero).
+Organiza tu proyecto con la siguiente estructura:
+css
+Copiar código
+mi_proyecto/
+    main.py
+    utils/
+        __init__.py
+        operaciones.py
+Importa y usa las funciones de operaciones.py en main.py para:
+Multiplicar dos números ingresados por el usuario.
+Dividir dos números ingresados por el usuario.
 """
-def decorador(funcion):
-    def nueva_funcion (*arg, **kwarg):
-        print("Inicio de la funcion")
-        result = funcion(*arg, **kwarg)
-        print("Fin de la funcion")
-        return result
-    return nueva_funcion
 
-@ decorador
-def sumar (a, b):
-    sum = a + b
-    print(f"La suma es: {sum}")
+# Solicitar números al usuario con manejo de errores
+def sol_num():
+    try:
+        a = int(input("Introduce un número a: "))
+        b = int(input("Introduce un número b: "))
+        return a, b
+    except ValueError:
+        print("Error: Debes introducir un número válido.")
+        return None
 
-sumar(5,3)
+# Realizar operaciones matemáticas
+def operaciones(a, b):
+    try:
+        suma = a + b
+        resta = a - b
+        multi = a * b
+        div = a / b
+        print(f"Resultados:\nSuma: {suma}\nResta: {resta}\nMultiplicación: {multi}\nDivisión: {div}")
+    except ZeroDivisionError:
+        print("Error: No se puede dividir entre cero.")
+
+# Función principal
+def main():
+    numeros = sol_num()
+    if numeros:  # Verifica si `sol_num()` devolvió una tupla válida
+        a, b = numeros
+        operaciones(a, b)
+        print (numeros)
+
+main()
